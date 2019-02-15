887df6c26546677d1e9ee6ec63a843dd24f47d30
baomihuahua <43314399+baomihuahua@users.noreply.github.com>
2019-02-15 18:46:44 +0800

    优化下邮件模板

--- modules/fun-comments.php
+++ modules/fun-comments.php
@@ -102,7 +102,7 @@ function comment_mail_notify($comment_id) {
     $subject = '您在 [' . get_option("blogname") . '] 的留言有了回复';
     $message = '
     <table style="width: 99.8%;height:99.8% "><tbody><tr><td style="background:#fafafa url()">
-<div style="border-radius: 10px 10px 10px 10px;font-size:13px;    color: #555555;width: 95%;font-family:"Century Gothic","Trebuchet MS","Hiragino Sans GB",微软雅黑,"Microsoft Yahei",Tahoma,Helvetica,Arial,"SimSun",sans-serif;margin:50px auto;border:1px solid #eee;max-width:100%;background: #ffffff repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);">
+<div style="border-radius: 10px 10px 10px 10px;font-size:13px;    color: #555555;width: 99%;font-family:"Century Gothic","Trebuchet MS","Hiragino Sans GB",微软雅黑,"Microsoft Yahei",Tahoma,Helvetica,Arial,"SimSun",sans-serif;margin:50px auto;border:1px solid #eee;max-width:100%;background: #ffffff repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);">
 <div style="width:100%;background:#49BDAD;color:#ffffff;border-radius: 10px 10px 0 0;background-image: -moz-linear-gradient(0deg, rgb(67, 198, 184), rgb(255, 209, 244));background-image: -webkit-linear-gradient(0deg, rgb(67, 198, 184), rgb(255, 209, 244));height: 66px;">
 <p style="font-size:15px;word-break:break-all;padding: 23px 32px;margin:0;background-color: hsla(0,0%,100%,.4);border-radius: 10px 10px 0 0;">
 您在<a style="text-decoration:none;color: #ffffff;" href="' . get_option('blogname') . '" rel="noopener" target="_blank"> [' . get_option('blogname') . '] </a> 的留言有回复啦！
