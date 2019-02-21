897f81857a8836c15fcdcc25ff34f94cc7de9f02
baomihuahua <43314399+baomihuahua@users.noreply.github.com>
2019-02-21 23:12:36 +0800

    代码高亮

--- options.php
+++ options.php
@@ -1,20 +1,8 @@
 <?php
-/**
- * A unique identifier is defined to store the options in the database and reference them from the theme.
- */
 function optionsframework_option_name() {
 	// Change this to use your theme slug
 	return 'options-framework-theme';
 }
-
-/**
- * Defines an array of options that will be used to generate the settings page and be saved in the database.
- * When creating the 'id' fields, make sure to use all lowercase and no spaces.
- *
- * If you are making your theme translatable, you should replace 'theme-textdomain'
- * with the actual text domain for your theme.  Read more:
- * http://codex.wordpress.org/Function_Reference/load_theme_textdomain
- */
 
 function optionsframework_options() {
 
