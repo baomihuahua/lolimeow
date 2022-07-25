50cbd3b128fda809d840cda7c3f0673dd6111fcb
专收爆米花 <504888738@qq.com>
2022-07-26 00:55:19 +0800

    调整下回复邮件的宽度

--- module/config/fun-comments.php
+++ module/config/fun-comments.php
@@ -86,7 +86,7 @@ function comment_mail_notify($comment_id) {
     $to = trim(get_comment($parent_id)->comment_author_email);
     $subject = '您在 [' . get_option("blogname") . '] 的留言有了回复';
     $message = '
-    <table style="width:60%;margin: 0 auto;height:100% "><tbody><tr><td style="background:#fafafa url()">
+    <table style="width:100%;margin: 0 auto;height:100% "><tbody><tr><td style="background:#fafafa url()">
 <div style="border-radius: 10px 10px 10px 10px;font-size:13px;    color: #555555;width:100%;font-family:"Century Gothic","Trebuchet MS","Hiragino Sans GB",微软雅黑,"Microsoft Yahei",Tahoma,Helvetica,Arial,"SimSun",sans-serif;margin:50px auto;border:1px solid #eee;max-width:100%;background: #ffffff repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);">
 <div style="width:100%;background:#49BDAD;color:#ffffff;border-radius: 10px 10px 0 0;background-image: -moz-linear-gradient(0deg, rgb(67, 198, 184), rgb(255, 209, 244));background-image: -webkit-linear-gradient(0deg, rgb(67, 198, 184), rgb(255, 209, 244));height: 66px;">
 <p style="font-size:15px;word-break:break-all;padding: 23px 32px;margin:0;background-color: hsla(0,0%,100%,.4);border-radius: 10px 10px 0 0;">
