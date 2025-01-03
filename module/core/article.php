7faa6ce83353c5ac614b77400fa0a787b3570b76
专收爆米花 <504888738@qq.com>
2025-01-04 00:52:35 +0800

    修复百度推送

--- module/core/article.php
+++ module/core/article.php
@@ -376,3 +376,24 @@ function boxmoe_remove_width_height($content) {
     return $content;
 }
 add_filter('the_content', 'boxmoe_remove_width_height', 99);
+
+if( get_boxmoe('baidutuisong') ){
+    if(!function_exists('Baidu_Submit')){
+        function Baidu_Submit($post_ID) {
+            $WEB_TOKEN  = get_boxmoe('baidutuisongkey');  //这里请换成你的网站的百度主动推送的token值
+            $WEB_DOMAIN = get_option('home');
+            //已成功推送的文章不再推送
+            if(get_post_meta($post_ID,'Baidusubmit',true) == 1) return;
+            $url = get_permalink($post_ID);
+            $api = 'http://data.zz.baidu.com/urls?site='.$WEB_DOMAIN.'&token='.$WEB_TOKEN;
+            $request = new WP_Http;
+            $result = $request->request( $api , array( 'method' => 'POST', 'body' => $url , 'headers' => 'Content-Type: text/plain') );
+            $result = json_decode($result['body'],true);
+            //如果推送成功则在文章新增自定义栏目Baidusubmit，值为1
+            if (array_key_exists('success',$result)) {
+                add_post_meta($post_ID, 'Baidusubmit', 1, true);
+            }
+        }
+        add_action('publish_post', 'Baidu_Submit', 0);
+    }
+    }
